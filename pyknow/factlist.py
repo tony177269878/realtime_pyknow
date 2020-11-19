2b08b7ad81e87411dd4064fe087c552ae460dfcd
tony177269878 <tony177269878@126.com>
2020-11-19 23:11:49 +0800

    ad upsert function to engine

--- pyknow/factlist.py
+++ pyknow/factlist.py
@@ -42,10 +42,13 @@ class FactList(OrderedDict):
 
     @staticmethod
     def _get_fact_id(fact):
-        return frozenset([fact.__class__]
-                         + [(k, v)
-                            for k, v in fact.items()
-                            if not fact.is_special(k)])
+        if 'level' in fact.keys() and 'id' in fact.keys():
+            return f"{fact['level']}_{fact['id']}"
+        else:
+            return frozenset([fact.__class__]
+                             + [(k, v)
+                                for k, v in fact.items()
+                                if not fact.is_special(k)])
 
     def declare(self, fact):
         """
